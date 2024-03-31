35d4ce255d09ebfea378a040bbc7dbaee4890699
PitPed <srpitped@gmail.com>
2024-04-01 01:33:03 +0200

    Fixed DB

--- mycook.sql
+++ mycook.sql
@@ -34,7 +34,8 @@ CREATE TABLE channels (
 -- Tabla measurements
 CREATE TABLE measurements (
     measurement_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    type ENUM('volume', 'weight')
 );
 
 -- Tabla methods
@@ -49,7 +50,7 @@ CREATE TABLE categories (
     name VARCHAR(255) NOT NULL
 );
 
--- Tabla recipes con quantity UNSIGNED
+-- Tabla recipes
 CREATE TABLE recipes (
     recipe_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     duration TIME NOT NULL,
@@ -60,7 +61,6 @@ CREATE TABLE recipes (
 -- Tabla posts
 CREATE TABLE posts (
     post_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    author VARCHAR(255) NOT NULL,
     date DATETIME DEFAULT CURRENT_TIMESTAMP,
     title VARCHAR(255) NOT NULL,
     body TEXT NOT NULL,
@@ -73,6 +73,7 @@ CREATE TABLE posts (
 -- Tabla comment
 CREATE TABLE comment (
     comment_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    date DATETIME DEFAULT CURRENT_TIMESTAMP,
     body TEXT NOT NULL,
     user_id INT UNSIGNED NOT NULL,
     post_id INT UNSIGNED NOT NULL,
@@ -80,8 +81,8 @@ CREATE TABLE comment (
     FOREIGN KEY (post_id) REFERENCES posts(post_id)
 );
 
--- Tabla image
-CREATE TABLE image (
+-- Tabla images
+CREATE TABLE images (
     image_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     url VARCHAR(255) NOT NULL,
     alt VARCHAR(255) NOT NULL,
@@ -91,30 +92,45 @@ CREATE TABLE image (
 
 -- Tabla members
 CREATE TABLE members (
+    member_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     user_id INT UNSIGNED NOT NULL,
-    group_id INT UNSIGNED NOT NULL,
+    channel_id INT UNSIGNED NOT NULL,
     rol ENUM('admin', 'manager', 'poster', 'member') NOT NULL,
-    PRIMARY KEY (user_id, group_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (channel_id) REFERENCES channels(channel_id)
 );
 
 -- Tabla post_channel
-CREATE TABLE post_channel (
+CREATE TABLE post_channels (
+    post_channel_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     post_id INT UNSIGNED NOT NULL,
     channel_id INT UNSIGNED NOT NULL,
-    PRIMARY KEY (post_id, channel_id),
     FOREIGN KEY (post_id) REFERENCES posts(post_id),
     FOREIGN KEY (channel_id) REFERENCES channels(channel_id)
 );
 
 -- Tabla recipe_ingredient
-CREATE TABLE recipe_ingredient (
+CREATE TABLE recipe_ingredients (
+    recipe_ingredient_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     recipe_id INT UNSIGNED NOT NULL,
     ingredient_id INT UNSIGNED NOT NULL,
     measurement_id INT UNSIGNED NOT NULL,
     quantity DECIMAL NOT NULL,
-    PRIMARY KEY (recipe_id, ingredient_id, measurement_id),
     FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id),
     FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id),
     FOREIGN KEY (measurement_id) REFERENCES measurements(measurement_id)
 );
+
+CREATE TABLE ingredient_categories(
+    ingredient_category_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    ingredient_id INT UNSIGNED NOT NULL,
+    category_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id),
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+
+-- Creación del usuario mycook_backend
+CREATE USER 'mycook_backend'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('MyC00k1324');
+
+-- Darle permisos en mycook
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES, CREATE VIEW, EVENT, TRIGGER, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, EXECUTE ON `mycook`.* TO 'mycook_backend'@'localhost';
